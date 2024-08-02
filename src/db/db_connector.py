d9b26baa7c8ed5b44f1b2ba6a17ad37e1cbe066e
surekhag28 <surekhag28@gmail.com>
2024-08-02 14:51:52 +1000

    added database connection changes

---
+++ src/db/db_connector.py
@@ -0,0 +1,47 @@
+import psycopg2
+from psycopg2 import OperationalError
+from src.config.config import *
+from src.logger import logging as logger
+
+class DatabaseConnection:
+    def __init__(self):
+        self.__connection = None
+        
+    def __connect(self):
+        """
+        Establishes a connection to the PostgresSQL database.
+        
+        Returns:
+            bool: True if the connection is successful, False otherwise
+        """
+        
+        try:
+            self.__connection = psycopg2.connect(
+                                    host = DBConfig.HOST,
+                                    port = DBConfig.PORT,
+                                    user = DBConfig.USER,
+                                    password = DBConfig.PASSWORD,
+                                    database = DBConfig.NAME
+            )
+            
+            return True
+        except OperationalError as e:
+            logger.error('Error connecting to the database: {}'.format(e)) 
+            return False
+        
+    def get_connection(self):
+        if self.__connection is None:
+            if not self.__connect():
+                raise Exception('Failed to establish database connection')
+        return self.__connection
+        
+    def close_connection(self):
+        if self.__connection:
+            try:
+                self.__connection.close()
+                self.__connection = None
+                logger.info('Database connection closed')
+            except Exception as e:
+                logger.error('Error closing the database connection: {}'.format(e))   
+            
+        
--- src/load_data/create_schema.py
+++ src/load_data/create_schema.py
@@ -1,20 +1,14 @@
 import os
 import sys
-import psycopg2
-import logging
 import pandas as pd
-from src.constants import *
-from src.config.config import DBConfig, DataIngestionConfig
+from src.constants import DB_SCHEMA
+from src.logger import logging as logger
+from src.config.config import DataIngestionConfig
+from src.db.db_connector import DatabaseConnection
+from src.exception import DatabaseConnectionError
 
-logger = logging.getLogger(__name__)
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(name)50s:%(lineno)4s - %(funcName)25s() ] - ( %(levelname)5s ) %(message)s",
-    handlers=[logging.FileHandler("create_schema.log"), logging.StreamHandler(sys.stdout)],
-)
-
-def create_db_schema(sql_file_path, global_conn):
+def create_db_schema(sql_file_path):
     
     """
     Creates a schema in the PostgreSQL database using a SQL script.
@@ -23,9 +17,10 @@ def create_db_schema(sql_file_path, global_conn):
         sql_file_path (str): Path to the SQL script file.
         global_conn (str): DB connection string.
     """
+    db_connector = DatabaseConnection()
     
     try:
-        conn = psycopg2.connect(global_conn)
+        conn = db_connector.get_connection()
         cursor = conn.cursor()
         
         with open(sql_file_path, 'r') as file:
@@ -33,21 +28,29 @@ def create_db_schema(sql_file_path, global_conn):
         
         cursor.execute(sql_script)
         conn.commit()
-
+        
     except FileNotFoundError as e:
         logger.error('Unable to find sql file at location: %s', sql_file_path)
         raise
     except Exception as e:
-        logger.error(f"An error occurred: {e}")
-        raise
+        logger.error('Database connection error: {}'.format(e))
+        raise DatabaseConnectionError(e, sys)
 
     finally:
         if cursor:
             cursor.close()
         if conn:
-            conn.close()
+            db_connector.close_connection()
             
 def load_data(file_path):
+    
+    """
+    Ingests data into table from csv file.
+
+    Parameters:
+        file_path (str): Path to the raw CSV data file.
+    """
+    
     df = pd.read_csv(file_path)
     data = df.values.tolist()
     
@@ -55,9 +58,11 @@ def load_data(file_path):
                     has_job_experience, requires_job_training, no_of_employees, yr_of_estab, \
                     region_of_employment, prevailing_wage, unit_of_wage, full_time_position, case_status)\
                     VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    conn = None
+    
+    db_connector = DatabaseConnection()
+    
     try:         
-        conn = psycopg2.connect(global_conn)
+        conn = db_connector.get_connection()
         
         cur = conn.cursor()
         del_statement = "DELETE FROM " + DB_SCHEMA + ".visadata"
@@ -65,16 +70,16 @@ def load_data(file_path):
         cur.executemany(sql01,data)
         conn.commit()
         conn.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+    except Exception as e:
+        logger.error('Database connection error: {}'.format(e))
+        raise DatabaseConnectionError(e, sys)
     finally:
         if conn is not None:
-            conn.close()
+            db_connector.close_connection()
             
 if __name__=='__main__':
     
-    global_conn = f'host={DBConfig.HOST} port={DBConfig.PORT} dbname={DBConfig.NAME} user={DBConfig.USER} password={DBConfig.PASSWORD}'
-    create_db_schema(os.path.join(os.path.dirname(__file__), 'db_schema.sql'), global_conn)
+    create_db_schema(os.path.join(os.path.dirname(__file__), 'db_schema.sql'))
     logger.info('Database created')
     
     load_data(DataIngestionConfig.raw_data_file_path)
